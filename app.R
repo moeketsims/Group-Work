8b5974ad16221e81de9d095526c76f4450f8b291
moeketsims <36858306+moeketsims@users.noreply.github.com>
2020-12-05 23:17:24 +0200

    Update app.R

--- app.R
+++ app.R
@@ -1,3 +1,27 @@
+NEO,MOLOI 202000784
+
+Imported the csv file
+
+Created the melt function
+
+Created the reactors for the graphs and table
+
+KWANELE,FADANE 202000003
+
+Created the dashboardHeader( The title)
+
+Created the dashbordSideBar( Select button and the slider button)
+
+Created the dashboardBody(The outputting of the graph and table )
+
+FAITH,LEBONE,MOTSHABI 201900815
+
+Created the output for the graphs and table
+
+Created the layout of the graphs (for example , coord_flop())
+
+Created the DT function
+
 library(shiny)
 library(shinydashboard)
 library(tidyverse)
